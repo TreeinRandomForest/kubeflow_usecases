adb8a9c9ae160407c936a8e7a99ccb5737dfbd9b
Juana Nakfour <nakfour>
2021-08-20 15:21:06 -0500

    Adding container for rpc

---
+++ Ch5/5.3 Remote Procedure Calls/rpc-test-container/rpc_test.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+import torch.optim as optim
+import torch.distributed as dist
+import torch.distributed.rpc as rpc
+from torch.distributed.rpc import RRef, rpc_async, rpc_sync, remote
+import torch.multiprocessing as mp
+import torch
+
+import os
+
+#python run.py --nnode 1 --nproc_per_node 2 rpc_rl.py --local_world_size 2
+
+key_list = ['MASTER_ADDR', 'MASTER_PORT', 'RANK', 'WORLD_SIZE']
+
+def run(rank, world_size):
+    print(f'rank = {rank} world_size = {world_size}')
+    env_dict = {key: os.environ[key] for key in key_list}
+
+    print(env_dict)
+
+    if rank==0:
+        rpc.init_rpc(f"rank{rank}", rank=rank, world_size=world_size,
+            backend=rpc.BackendType.TENSORPIPE)
+        
+        #rref = rpc.rpc_async('rank1', torch.sum, args=(torch.ones(2),))
+        rref = rpc.remote('rank1', torch.sum, args=(torch.ones(2),))
+        print(rref)
+        print(rref.to_here())  
+
+    else:
+        rpc.init_rpc(f"rank{rank}", rank=rank, world_size=world_size,
+            backend=rpc.BackendType.TENSORPIPE)
+
+    rpc.shutdown()
+
+if __name__=='__main__':
+    rank = int(os.environ['RANK'])
+    world_size = int(os.environ['WORLD_SIZE'])
+
+    run(rank, world_size)
