716ef773ba61267c201ddd3c1270673b38db5671
treeinrandomforest <sra2114@gmail.com>
2021-09-02 17:10:37 -0400

    pass env_name to animate

--- Ch5/5.3 Remote Procedure Calls/utils.py
+++ Ch5/5.3 Remote Procedure Calls/utils.py
@@ -46,7 +46,9 @@ def plot_learning_curve(filename):
     d = json.load(open(filename, 'rb'))
 
     plt.figure()
-    plt.plot(d.keys(), d.values(), label=filename)
+    plt.plot(d.keys(), [v[0] for v in d.values()], label=f'{filename}: batch')
+    plt.plot(d.keys(), [v[1] for v in d.values()], label=f'{filename}: exp avg')
+
     plt.xlabel('Time (s)')
     plt.ylabel('Average Reward in Current Batch')
 
@@ -54,20 +56,20 @@ def plot_learning_curve(filename):
     ticks = np.arange(0, len(time_vals), 100)
     labels = [format(float(t), ".1f") for t in time_vals[ticks]]
 
-    plt.xticks(ticks=ticks, labels=labels)
+    plt.xticks(ticks=ticks, labels=labels, rotation=90)
 
     plt.legend()
-    
+
     return d
 
-def animate(policy_filename):
+def animate(policy_filename, env_name='CartPole-v1'):
     '''Can be combined with create trajectories above
     '''
     policy = torch.load(open(policy_filename, 'rb'))
 
     #env specific
-    env = gym.make('CartPole-v1')
-    action_space = np.arange(0, 2)
+    env = gym.make(env_name)
+    action_space = np.arange(0, env.action_space.n)
 
     state = env.reset()
 
